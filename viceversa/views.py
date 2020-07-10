8c7281e7274657517b655fd6272f7e2dda1d2e05
Parvin <pervin.guseynov@gmail.com>
2020-07-10 17:24:11 +0400

    Third commit

--- viceversa/views.py
+++ viceversa/views.py
@@ -12,6 +12,9 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    text_split = user_text.split()
+    len_element = len(text_split)
+    print(len_element)
     reversed_text = user_text[::-1]
 
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'len': len_element})
